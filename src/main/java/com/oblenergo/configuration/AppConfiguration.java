df92d0534e509096d59e62c7fc19bb618724cf02
yuralubinec <yuralubinec@gmail.com>
2017-03-10 12:33:52 +0200

    removed useless annotations

--- src/main/java/com/oblenergo/configuration/AppConfiguration.java
+++ src/main/java/com/oblenergo/configuration/AppConfiguration.java
@@ -1,19 +1,12 @@
 package com.oblenergo.configuration;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan("com.oblenergo")
 public class AppConfiguration extends WebMvcConfigurerAdapter {
 
   @Override
@@ -31,12 +24,12 @@ public class AppConfiguration extends WebMvcConfigurerAdapter {
     registry.viewResolver(viewResolver);
   }
 
-  @Bean
-  public MessageSource messageSource() {
-
-    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-    messageSource.setBasename("validationMessage");
-    return messageSource;
-  }
+//  @Bean
+//  public MessageSource messageSource() {
+//
+//    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//    messageSource.setBasename("validationMessage");
+//    return messageSource;
+//  }
 
 }
--- src/main/java/com/oblenergo/configuration/UraganSpringBootApplication.java
+++ src/main/java/com/oblenergo/configuration/UraganSpringBootApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class }, scanBasePackages = "com.oblenergo")
 public class UraganSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UraganSpringBootApplication.class, args);
-	}
-	
-	
+  public static void main(String[] args) {
+    SpringApplication.run(UraganSpringBootApplication.class, args);
+  }
+
 }
